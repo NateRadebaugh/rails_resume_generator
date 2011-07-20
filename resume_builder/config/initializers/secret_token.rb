daedc0ef436bf53831d5574009075e084e77e66b
Nate Radebaugh <nate.radebaugh@gmail.com>
2011-07-19 22:38:16 -0700

    new rails project

---
+++ resume_builder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResumeBuilder::Application.config.secret_token = 'c816c895fd8120e1d3a86625e8fea335e18e2e9dc404bd5a19c8ed40e233f040894fb4f946bc6995bf6a6eec27e65d4765fa359504af6f0dfb9a6579f3321693'
